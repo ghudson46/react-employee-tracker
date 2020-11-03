0ea0b6514afc09f8b6e68af8d5fca39e8a551eb9
Garrett Hudson <garretthudson87@yahoo.com>
2020-11-02 22:10:03 -0500

    created footer component

---
+++ employee-tracker/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Footer.css'
+
+
+function Footer() {
+    return (
+        <>
+            <footer className="footer">
+                <div className="row m-0">
+                    <div className="col-md-12 text-center p-4">
+                        Copyright &copy; 2020 Garrett Hudson
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+
+export default Footer
